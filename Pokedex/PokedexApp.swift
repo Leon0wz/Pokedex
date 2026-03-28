0465ef68ffe311ef164e07802fb0bf20c841b043
Leon <zimnyleon@icloud.com>
2026-03-28 08:01:44 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Leon Zimny on 28.03.26.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
